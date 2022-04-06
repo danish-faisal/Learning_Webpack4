376c4c480c33b7ebd817f0f39783d325d3b4eae2
= <danishfaisal.wwe@gmail.com>
2022-04-06 11:35:50 +0530

    Run webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const $ = require("jquery");
+
+$("#target").html("Hello World");
+
+// to run webpack -> ./node_modules/.bin/webpack (or) just "webpack" if installed globally
+// or run "npx webpack" -> will create the main.js file in /dist
